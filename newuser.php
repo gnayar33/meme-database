19533ff2fb58ecd58ae18bd1fb7c49303998b8bc
njiang1209 <njiang1209@ufl.edu>
2014-04-07 22:04:21 -0400

    update

--- newuser.php
+++ newuser.php
@@ -1,16 +1,20 @@
 #!/usr/local/bin/php
 <?PHP
+/*
 	ob_clean();
 	$username = ($_GET['username']);
 	$password = ($_GET['password']);
-
+	$email = ($_GET['email']);
+	$phone = ($_GET['phone']);
+*/
+/*
 	$conn  = pg_connect('user=njiang host=postgres dbname=db password=asdfasdf');
 
 	if (!$conn) { 
 		echo "ConError";
 		exit;
 	}
-	$query = sprintf("select password from users where username = '" . $username .  "'");
+	$query = sprintf("select * from users where username = '" . $username .  "'");
 	$result = pg_query($conn, $query);
 
 	if (!$result) {
@@ -20,13 +24,14 @@
 
 	
 	if (pg_fetch_all($result) == false) {
-		echo "NOTFOUND";
+		//$query = sprintf("insert into users values (DEFAULT, '" + $username + "','" + $password +  "','" + $email + "','" + $phone + "')");
+		echo($query);
+		
 		exit;
 	} else {
-		$arr = pg_fetch_array ($result, 0, PGSQL_ASSOC);
-		if ($arr['password'] == $password)
-			echo "VALID";
-		else
-			echo "INVALID";	
+		echo "EXISTS";
+		exit;
 	}
+	
+	*/
 ?>
