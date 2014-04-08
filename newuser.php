a17684fa33b4971abd68cd8a3906c7c89b79f40f
njiang1209 <njiang1209@ufl.edu>
2014-04-07 22:20:16 -0400

    update

--- newuser.php
+++ newuser.php
@@ -1,13 +1,12 @@
 #!/usr/local/bin/php
 <?PHP
-/*
+
 	ob_clean();
 	$username = ($_GET['username']);
 	$password = ($_GET['password']);
 	$email = ($_GET['email']);
-	$phone = ($_GET['phone']);
-*/
-/*
+	$phone = $_GET['phone'];
+
 	$conn  = pg_connect('user=njiang host=postgres dbname=db password=asdfasdf');
 
 	if (!$conn) { 
@@ -24,14 +23,20 @@
 
 	
 	if (pg_fetch_all($result) == false) {
-		//$query = sprintf("insert into users values (DEFAULT, '" + $username + "','" + $password +  "','" + $email + "','" + $phone + "')");
-		echo($query);
-		
+		$query = sprintf("insert into users values (DEFAULT, '" . $username . "','"  . $email . "','" .  $password . "','" . $phone . "')");
+
+		$result = pg_query($conn, $query);
+		if (!$result) {
+			echo "QueryError";
+			exit;
+		}
+
+		echo "SUCCESS";
 		exit;
 	} else {
 		echo "EXISTS";
 		exit;
 	}
 	
-	*/
+
 ?>
